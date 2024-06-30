da16bb05807a4761fcce8aa8b343d94edd6b19d3
Ishanga Vidusha <ishangavidusha@gmail.com>
2024-06-30 16:07:07 +0530

    Set initial value for FilterState to the filter's initial value

--- lib/src/filter_state.dart
+++ lib/src/filter_state.dart
@@ -5,5 +5,5 @@ final class FilterState<T extends Object> {
   final TableFilter<T> _filter;
   T? value;
 
-  FilterState._(this._filter);
+  FilterState._(this._filter) : value = _filter.initialValue; // Set the initial value to the filter's initial value.
 }
