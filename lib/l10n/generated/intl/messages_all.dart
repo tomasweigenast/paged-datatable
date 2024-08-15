54fd01d05baaf0788d254080504e517798b67eac
bymoye <964907582@qq.com>
2024-08-15 10:50:26 +0800

    feat: add zh locale

--- lib/l10n/generated/intl/messages_all.dart
+++ lib/l10n/generated/intl/messages_all.dart
@@ -20,6 +20,7 @@ import 'messages_de.dart' as messages_de;
 import 'messages_en.dart' as messages_en;
 import 'messages_es.dart' as messages_es;
 import 'messages_it.dart' as messages_it;
+import 'messages_zh.dart' as messages_zh;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
@@ -27,6 +28,7 @@ Map<String, LibraryLoader> _deferredLibraries = {
   'en': () => new SynchronousFuture(null),
   'es': () => new SynchronousFuture(null),
   'it': () => new SynchronousFuture(null),
+  'zh': () => new SynchronousFuture(null),
 };
 
 MessageLookupByLibrary? _findExact(String localeName) {
@@ -39,6 +41,8 @@ MessageLookupByLibrary? _findExact(String localeName) {
       return messages_es.messages;
     case 'it':
       return messages_it.messages;
+    case 'zh':
+      return messages_zh.messages;
     default:
       return null;
   }
